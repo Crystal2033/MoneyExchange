095997d5c50e29ed553301b3dd1239ae4beddb81
Pavel <kulikov-pavlik@bk.ru>
2022-10-28 21:16:59 +0300

    removed some warnings

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,7 @@
-import Colors.ConsoleColors;
 import Exceptions.ImpossibleToExchange;
 import Exceptions.WrongInputData;
 import MyCollections.Pair;
-import com.sun.source.tree.Tree;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeSet;
 
@@ -15,24 +12,21 @@ public class Application {
         System.out.println("This program is able to exchange your money in denominations you want if it possible.");
         System.out.println();
         ArgsConverter argsConverter = new ArgsConverter();
-        Pair<Integer, TreeSet<Integer>> moneyValueAndDenominations = null;
+        Pair<Integer, TreeSet<Integer>> moneyValueAndDenominations;
         try {
             moneyValueAndDenominations = argsConverter.getMoneyAndDenominations(args);
-        }
-        catch(NumberFormatException | WrongInputData numberFormatException){
+        } catch (NumberFormatException | WrongInputData numberFormatException) {
             System.out.println(numberFormatException.getMessage());
             return;
         }
         try {
             if (moneyValueAndDenominations == null) throw new AssertionError();
             MoneyExchanger moneyExchanger = new MoneyExchanger(moneyValueAndDenominations.getSecond());
-            Map<Integer, Integer> exchangedMoney =  moneyExchanger.exchange(moneyValueAndDenominations.getFirst());
-            exchangedMoney.forEach((nominal, quantity)-> System.out.println(nominal + "[" + quantity + "]"));
-        }
-        catch (ImpossibleToExchange impossibleToExchange){
+            Map<Integer, Integer> exchangedMoney = moneyExchanger.exchange(moneyValueAndDenominations.getFirst());
+            exchangedMoney.forEach((nominal, quantity) -> System.out.println(nominal + "[" + quantity + "]"));
+        } catch (ImpossibleToExchange impossibleToExchange) {
             System.out.println(impossibleToExchange.getMessage());
-        }
-        catch(WrongInputData wrongInputData){
+        } catch (WrongInputData wrongInputData) {
             System.out.println(wrongInputData.getMessage());
         }
     }
